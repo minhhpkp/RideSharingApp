d15401a85a00aac95b73b48cfc9c5c37570b48cc
dzgod1905 <99169476+dzgod1905@users.noreply.github.com>
2023-12-06 13:32:03 +0700

    feat: MessageNotification added

--- passengersideapp/src/main/java/com/ridesharingapp/passengersideapp/splashscreen/SplashFragment.kt
+++ passengersideapp/src/main/java/com/ridesharingapp/passengersideapp/splashscreen/SplashFragment.kt
@@ -13,8 +13,6 @@ import com.ridesharingapp.passengersideapp.RideSharingApp
 
 class SplashFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val app = requireActivity().application as RideSharingApp
-        app.service.showNotification("Bạn có tin nhắn mới","", com.ridesharingapp.common.R.drawable.message)
         return ComposeView(requireContext()).apply {
             // Dispose the Composition when the view's LifecycleOwner is destroyed
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
