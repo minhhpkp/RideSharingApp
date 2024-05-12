88abdb185472f5f329bb8af54cb9815d41440297
Minh <phamquangminh1659@gmail.com>
2024-05-13 00:04:52 +0700

    Integrate SonarCloud into Github CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,23 @@ plugins {
     id("com.android.library") version "8.1.4" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
     id ("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version("2.0.1") apply false
+    id("org.sonarqube") version "5.0.0.4638"
 }
 
 buildscript {
     dependencies {
         classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+    }
+}
+
+sonar {
+//    val properties = java.util.Properties().apply {
+//        load(rootProject.file("local.properties").reader())
+//    }
+//    val mySecret = properties["SONAR_TOKEN"] as String
+    properties {
+        property("sonar.projectKey", "minhhpkp_RideSharingApp")
+        property("sonar.organization", "minhhpkp")
+        property("sonar.host.url", "https://sonarcloud.io")
     }
 }
